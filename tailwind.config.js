9da49dc6c5eef2289c1e2892adf3b67e1dc4ab42
Drey <dareybee@gmail.com>
2022-10-30 13:36:04 -0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        public: "'Public Sans', sans-serif"
+      },
+    },
   },
   plugins: [],
 }
